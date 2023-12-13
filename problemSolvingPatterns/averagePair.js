77cec9884d6c09fa1b1a9fa53e4038c4c09a1d54
Aidan Simmons <aidanwsimmons621@gmail.com>
2023-12-12 17:58:00 -0800

    multiple pointers solution

---
+++ problemSolvingPatterns/averagePair.js
@@ -0,0 +1,37 @@
+// Write a function called averagePair. Given a sorted array of integers and 
+// a target average, determine if there is a pair of values in the array where 
+// the average of the pair equals the target average. 
+// There may be more than one pair that matches the average target.
+
+// Bonus Constraints:
+
+// Time: O(N)
+
+// Space: O(1)
+
+// Sample Input:
+
+// averagePair([1,2,3],2.5) // true
+// averagePair([1,3,3,5,6,7,10,12,19],8) // true
+// averagePair([-1,0,3,4,5,6], 4.1) // false
+// averagePair([],4) // false
+
+
+function averagePair(arr, average){
+    let left = 0
+    let right = arr.length - 1
+    
+    while(left < right){
+        let tempAverage = (arr[left] + arr[right]) / 2
+        if(tempAverage === average){
+            return true
+        }
+        else if(tempAverage > average){
+            right--
+        }
+        else if(tempAverage < average){
+            left++
+        }
+    }
+    return false
+  }
