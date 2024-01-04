82c4371897fa18d11c065cbbae31356e669a9363
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-01-04 11:50:21 -0800

    binary search implementation

---
+++ serchingAlgorithms/binarySearch.js
@@ -0,0 +1,26 @@
+function binarySearch(arr, val){
+    let min = 0
+    let max = arr.length - 1
+    let mid = Math.floor((max + min) / 2)
+    
+    if(val < arr[min] || val > arr[max]){
+        console.log('out of range')
+        return -1
+    }
+    
+    while(max >= min){
+        console.log(min, max, mid)
+        if(arr[mid] === val){
+            return mid
+        }
+        else if(arr[mid] > val){
+            max = mid - 1
+        }
+        else if(arr[mid] < val){
+            min = mid + 1
+        }
+        mid = Math.floor((max + min) / 2)
+    }
+    console.log('num not found')
+    return -1
+}
