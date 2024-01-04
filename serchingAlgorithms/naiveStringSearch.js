a519d8333c42c5f1c27b99e0764e0fb438ae8d43
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-01-04 12:18:40 -0800

    implementaion of anive string search

---
+++ serchingAlgorithms/naiveStringSearch.js
@@ -0,0 +1,17 @@
+//function accepts a string and a substring. 
+//returns number of times substring appears in string
+
+function naiveSearch(long, short){
+    let count = 0
+    for(let i = 0; i < long.length; i++){
+            for(let j = 0; j < short.length; j++){
+                if(short[j] !== long[i+j]){
+                    break;
+                }
+                if(j === short.length - 1){
+                    count++
+                }
+            }
+        }
+        return count
+    }
