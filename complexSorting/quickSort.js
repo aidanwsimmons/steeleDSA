a09a7b9a69e177618cef211657044b6d5e1220e7
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-01-17 15:59:04 -0800

    fixed pivot helper to utilize end param, and quick sort implementation

--- complexSorting/quickSort.js
+++ complexSorting/quickSort.js
@@ -1,10 +1,3 @@
-//given an array, designate an element as the pivot and rerarrange elements in the array so that all
-//values less than the pivot are moved left of the pivot, and all values greater than the pivot
-//are moved to the right of the pivot
-//in place
-//returns pivot index
-
-
 function pivotHelper(arr, start=0, end=arr.length-1){
     function swap(array, i, j){
         let temp = array[i]
@@ -23,3 +16,14 @@ function pivotHelper(arr, start=0, end=arr.length-1){
     swap(arr, start, swapIndex)
     return swapIndex
 }
+
+ function quickSort(arr, left = 0, right = arr.length - 1){
+    if(left < right){
+    let pivotIndex = pivot(arr, left, right)
+    //left side
+    quickSort(arr,left, pivotIndex - 1)
+    //right side
+    quickSort(arr, pivotIndex + 1, right)
+    }
+    return arr
+ }
