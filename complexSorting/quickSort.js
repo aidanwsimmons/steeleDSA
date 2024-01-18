70ed66620365edb288ec9c19764dab84478764a1
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-01-17 16:02:31 -0800

    fixed naming error in pivot helper call

--- complexSorting/quickSort.js
+++ complexSorting/quickSort.js
@@ -19,7 +19,7 @@ function pivotHelper(arr, start=0, end=arr.length-1){
 
  function quickSort(arr, left = 0, right = arr.length - 1){
     if(left < right){
-    let pivotIndex = pivot(arr, left, right)
+    let pivotIndex = pivotHelper(arr, left, right)
     //left side
     quickSort(arr,left, pivotIndex - 1)
     //right side
