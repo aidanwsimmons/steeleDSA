08ddd05a21c3deaff7c6f801fd54330402cc0881
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-02-12 14:12:31 -0800

    implemented depthFirst and breathFirst search methods

---
+++ graphTraversal/graphTraversal.js
@@ -0,0 +1,72 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex] = [];
+        }
+    }
+    addEdge(vertex1, vertex2){
+        if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+            this.adjacencyList[vertex1].push(vertex2)
+            this.adjacencyList[vertex2].push(vertex1)
+        }
+    }
+    removeEdge(vertex1, vertex2){
+        if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+            this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+             vertex => vertex !== vertex2
+            )
+            this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+                vertex => vertex !== vertex1
+            )
+        }
+    }
+    removeVertex(vertex){
+        if(this.adjacencyList[vertex]){
+            while(this.adjacencyList[vertex].length){
+                 const adjacentVertex = this.adjacencyList[vertex].pop();
+                 this.adjacencyList.removeEdge(vertex, adjacentVertex)
+            }
+            delete this.adjacencyList[vertex]
+        }
+    }
+
+    depthFirstRecursive(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            })
+        })(start)
+
+        return result;
+        
+    }
+}
+
+let g = new Graph();
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
